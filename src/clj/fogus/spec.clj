11085c7f2fd1b0633e148637acd3a585981827df
Fogus <mefogus@gmail.com>
2021-09-09 12:40:42 -0400

    spec fail case for kwargs

--- src/clj/fogus/spec.clj
+++ src/clj/fogus/spec.clj
@@ -2,25 +2,28 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.spec.test.alpha :as stest]))
 
-(comment
-
-(defn foo [& {:keys [a b]}] [a b])
-
-(foo :a 1 :b 2)
-;;=> [1 2]
-
-(foo {:a 1 :b 2})
-;;=> [1 2]
+(defn foo [& {:keys [a b]}]
+  [a b])
 
 (s/def ::a any?)
 (s/def ::b any?)
+
 (s/fdef foo :args (s/keys* :opt [::a ::b]))
+                  ;; keys* not sufficient here
 
-(stest/instrument `foo)
+(comment
 
-(foo :a "a" :b "b")
-;;=> ["a" "b"]
-
-(foo {:a "a" :b "b"})
+  (foo :a 1 :b 2)
+  ;;=> [1 2]
+  
+  (foo {:a 1 :b 2})
+  ;;=> [1 2]
+  
+  (stest/instrument `foo)
+  
+  (foo :a "a" :b "b")
+  ;;=> ["a" "b"]
+  
+  (foo {:a "a" :b "b"})
 
 )
