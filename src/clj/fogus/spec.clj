19ac94365ff726f0ecf74ff6f705d7601d2bed55
Fogus <mefogus@gmail.com>
2021-09-15 14:36:15 -0400

    first form of keys*+

--- src/clj/fogus/spec.clj
+++ src/clj/fogus/spec.clj
@@ -30,23 +30,46 @@
   [& kspecs]
   `(let [mspec# (s/keys ~@kspecs)]
      (s/with-gen (s/& (s/* (s/cat ::k keyword? ::v any?)) ::kvs->map mspec#)
-       (fn [] (gen/fmap (fn [m#] (apply concat m#)) (s/gen mspec#))))))
+       (fn [] (gen/fmap (fn [m#] (apply concat m#))
+                        (s/gen mspec#))))))
 
 (defmacro keys*2
   [& kspecs]
-  `(s/cat :kvs (s/keys* ~@kspecs)
+  `(s/cat :kvs (s/* (s/cat :k keyword? :v any?)) ;;(s/keys* ~@kspecs)
           :trailing (s/? map?)))
+
+(s/def ::maps->map (s/conformer #(merge (:kvs %) (:trailing %))))
+
+(defmacro keys*+
+  [& kspecs]
+  `(s/conformer
+    #(->> % 
+          (s/conform (s/cat :kvs (s/keys*) :trailing (s/? map?)))
+          (s/conform ::maps->map)
+          (s/conform (s/keys ~@kspecs)))))
 
 (comment
 
   (s/conform (keys*2 :req-un [::a ::c])
+             [:a 1 :c 2])
+  
+  (s/conform (keys*2 :req-un [::a ::c])
              [:a 1 :c 2 {:b 3}])
+
+  (s/conform (keys*2 :req-un [::a ::c])
+             [{:b 3}])
+
+  (s/conform (keys*+ :req-un [::a ::c])
+             [:a 1 :c 2])
+  
+  (s/conform (keys*+ :req-un [::a ::c])
+             [:a 1 :c 2 {:b 3}])
+
+  (s/conform (keys*+ :req-un [::a ::c])
+             [{:a 1 :c 3}])
   
   (s/conform (keys* :req-un [::a ::c])
-             [:a 1 :c 2])
-
-  (s/conform ::TM [:a 1 :b 2 {:c 3}])
-
+             [{:b 3}])
 )
 
 (defn add [& {:keys [a b]}] (+ a b))
@@ -76,4 +99,25 @@
   (add {:a 1 :b 2})
 
   (add :a 1 {:b 2})
+)
+
+
+
+
+(defmacro keys*
+  [& kspecs]
+  `(let [mspec# (s/keys ~@kspecs)]
+     (s/with-gen (s/& (s/* (s/cat ::k keyword? ::v any?)) ::kvs->map mspec#)
+       (fn [] (gen/fmap (fn [m#] (apply concat m#))
+                        (s/gen mspec#))))))
+
+(comment
+
+  (def v2 {:kvs {:a 1, :c 2}, :trailing {:b 3}})
+
+
+  (->> [:a 1 :c 3 {:b 2}]
+       (s/conform (s/cat :kvs (s/keys*) :trailing (s/? map?)))
+       (s/conform ::maps->map)
+       (s/conform (s/keys :req-un [::a ::c])))
 )
