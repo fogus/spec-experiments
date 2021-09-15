79795a9bbe342e14994fdcf2f40ce396d3c86170
Fogus <mefogus@gmail.com>
2021-09-15 14:39:34 -0400

    first form of keys*+, cleaned ns

--- src/clj/fogus/spec.clj
+++ src/clj/fogus/spec.clj
@@ -9,34 +9,8 @@
 (s/def ::a any?)
 (s/def ::b any?)
 
-(s/fdef foo :args (s/keys* :opt [::a ::b]))
 ;; keys* not sufficient here
-
-(def kvs<-map #(map (fn [[k v]] {::k k ::v v}) %))
-
-(s/def ::kvs->map (s/conformer #(zipmap (map ::k %) (map ::v %)) kvs<-map))
-(s/def ::lift-map (s/conformer identity list))
-(s/def ::kvs->map (s/conformer #(zipmap (map ::k %) (map ::v %)) #(map (fn [[k v]] {::k k ::v v}) %)))
-
-(defmacro kvs+tm [& kspecs]
-  `(let [mspec# (s/keys ~@kspecs)]
-     (s/with-gen (clojure.spec.alpha/&
-                  (s/alt :m (clojure.spec.alpha/& mspec# ::lift-map)
-                         :s (clojure.spec.alpha/& (s/* (s/cat ::k keyword? ::v any?)) ::kvs->map mspec#))
-                  (s/conformer second #(map (fn [[k# v#]] {::k k# ::v v#}) %)))
-       (fn [] (gen/fmap (fn [m#] (apply concat m#)) (s/gen mspec#))))))
-
-(defmacro keys*
-  [& kspecs]
-  `(let [mspec# (s/keys ~@kspecs)]
-     (s/with-gen (s/& (s/* (s/cat ::k keyword? ::v any?)) ::kvs->map mspec#)
-       (fn [] (gen/fmap (fn [m#] (apply concat m#))
-                        (s/gen mspec#))))))
-
-(defmacro keys*2
-  [& kspecs]
-  `(s/cat :kvs (s/* (s/cat :k keyword? :v any?)) ;;(s/keys* ~@kspecs)
-          :trailing (s/? map?)))
+(s/fdef foo :args (s/keys* :opt [::a ::b]))
 
 (s/def ::maps->map (s/conformer #(merge (:kvs %) (:trailing %))))
 
@@ -76,7 +50,7 @@
 
 (s/def ::a any?)
 (s/def ::c any?)
-(s/fdef add :args (kvs+tm :opt [::a ::c]))
+(s/fdef add :args (keys*+ :opt [::a ::c]))
 (stest/instrument `add)
 
 (comment
@@ -99,25 +73,4 @@
   (add {:a 1 :b 2})
 
   (add :a 1 {:b 2})
-)
-
-
-
-
-(defmacro keys*
-  [& kspecs]
-  `(let [mspec# (s/keys ~@kspecs)]
-     (s/with-gen (s/& (s/* (s/cat ::k keyword? ::v any?)) ::kvs->map mspec#)
-       (fn [] (gen/fmap (fn [m#] (apply concat m#))
-                        (s/gen mspec#))))))
-
-(comment
-
-  (def v2 {:kvs {:a 1, :c 2}, :trailing {:b 3}})
-
-
-  (->> [:a 1 :c 3 {:b 2}]
-       (s/conform (s/cat :kvs (s/keys*) :trailing (s/? map?)))
-       (s/conform ::maps->map)
-       (s/conform (s/keys :req-un [::a ::c])))
 )
