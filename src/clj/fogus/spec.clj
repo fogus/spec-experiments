3a5f7d5bad467076e1c99442483c6bca6814a0d9
Fogus <mefogus@gmail.com>
2021-09-27 09:53:36 -0400

    pulling in relevant instrument fns

--- src/clj/fogus/spec.clj
+++ src/clj/fogus/spec.clj
@@ -75,6 +75,11 @@
   (add {:a 1 :b 2})
 
   (add :a 1 {:b 2})
+
+  (defmacro qux [& {:keys [a]}]
+    a)
+
+  (stest/instrument `qux)
 )
 
 (defn- xform-keys*
@@ -92,4 +97,84 @@
 
   (xform-keys* `(s/cat :kwargs (s/keys* :opt-un [::a ::c])))
 
+  (get (s/registry) 'fogus.spec/add)
+  
+  (-> (s/registry)
+      (get 'fogus.spec/add)
+      (get :args)
+      s/describe)
+
+  @(deref #'clojure.spec.test.alpha/instrumented-vars)
+
+  (meta #'foo)
 )
+
+(defn bar
+  ([opts] opts)
+  ([a b] [a b])
+  ([a b & {:as m}] m))
+
+(meta #'bar)
+
+(def al '([opts] [a b] [a b & {:as m}]))
+
+(sort-by count al)
+
+(use 'clojure.spec.test.alpha)
+
+(def instrument-choose-spec (deref #'clojure.spec.test.alpha/instrument-choose-spec))
+(def instrument-choose-fn (deref #'clojure.spec.test.alpha/instrument-choose-fn))
+(def no-fspec (deref #'clojure.spec.test.alpha/no-fspec))
+(def stacktrace-relevant-to-instrument (deref #'clojure.spec.test.alpha/stacktrace-relevant-to-instrument))
+
+(def ^:private ^:dynamic *instrument-enabled*
+  "if false, instrumented fns call straight through"
+  true)
+
+(defonce ^:private instrumented-vars (atom {}))
+
+(defn- spec-checking-fn
+  [v f fn-spec]
+  (let [fn-spec (@#'s/maybe-spec fn-spec)
+        conform! (fn [v role spec data args]
+                   (let [conformed (s/conform spec data)]
+                     (if (= ::s/invalid conformed)
+                       (let [caller (->> (.getStackTrace (Thread/currentThread))
+                                         stacktrace-relevant-to-instrument
+                                         first)
+                             ed (merge (assoc (s/explain-data* spec [] [] [] data)
+                                         ::s/fn (->sym v)
+                                         ::s/args args
+                                         ::s/failure :instrument)
+                                       (when caller
+                                         {::caller (dissoc caller :class :method)}))]
+                         (throw (ex-info
+                                 (str "Call to " v " did not conform to spec.")
+                                 ed)))
+                       conformed)))]
+    (fn
+     [& args]
+     (if *instrument-enabled*
+       (with-instrument-disabled
+         (when (:args fn-spec) (conform! v :args (:args fn-spec) args args))
+         (binding [*instrument-enabled* true]
+           (.applyTo ^clojure.lang.IFn f args)))
+       (.applyTo ^clojure.lang.IFn f args)))))
+
+(defn- instrument-1
+  [s opts]
+  (when-let [v (resolve s)]
+    (when-not (-> v meta :macro)
+      (let [spec (s/get-spec v)
+            {:keys [raw wrapped]} (get (deref #'clojure.spec.test.alpha/instrumented-vars) v)
+            current @v
+            to-wrap (if (= wrapped current) raw current)
+            ospec (or (instrument-choose-spec spec s opts)
+                      (throw (no-fspec v spec)))
+            ofn (instrument-choose-fn to-wrap ospec s opts)
+            checked (spec-checking-fn v ofn ospec)]
+        (alter-var-root v (constantly checked))
+        (swap! instrumented-vars assoc v {:raw to-wrap :wrapped checked})
+        (->sym v)))))
+
+
