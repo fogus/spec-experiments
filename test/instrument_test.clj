5577643fed0491baef7d4f140c6d8b4ec5cf60f5
Fogus <mefogus@gmail.com>
2021-10-06 13:39:13 -0400

    starting tests

---
+++ test/instrument_test.clj
@@ -0,0 +1,42 @@
+(ns instrument-test
+  (:require [clojure.test :refer :all]
+            [clojure.spec.alpha :as s]
+            [fogus.instr :as stest]))
+
+(defn kwargs-fn
+  ([opts] opts)
+  ([a b] [a b])
+  ([a b & {:as m}] [a b m]))
+
+(defn no-kwargs-fn
+  ([opts] opts)
+  ([a b] [a b])
+  ([a b & opts] [a b opts]))
+
+(defn just-varargs [& args]
+  (apply + )args)
+
+;;; Specs
+
+(s/def ::a any?)
+(s/def ::b number?)
+(s/def ::c any?)
+
+(s/fdef kwargs-fn
+  :args (s/alt :unary  (s/cat :a ::a)
+               :binary (s/cat :a ::a :b ::b)
+               :variadic (s/cat :a ::a
+                                :b ::b
+                                :kwargs (s/keys* :opt-un [::a ::b ::c]))))
+
+(s/fdef no-kwargs-fn
+  :args (s/alt :unary  (s/cat :a ::a)
+               :binary (s/cat :a ::a :b ::b)
+               :variadic (s/cat :a ::a
+                                :b ::b
+                                :varargs (s/cat :numbers (s/* number?)))))
+
+(s/fdef just-varargs
+  :args (s/cat :numbers (s/* number?))
+  :ret number?)
+
